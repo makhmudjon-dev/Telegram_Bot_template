c14a37154dd3661e0799d565377176ba093a0211
makhmud <makhmud.dev09@gmail.com>
2024-02-12 16:47:56 +0500

    adding db

--- src/handlers/user/default.py
+++ src/handlers/user/default.py
@@ -1,16 +1,21 @@
-from keyboards.main import inline_builder
 from typing import NoReturn
 
-from aiogram import Router
-from aiogram.types import Message
-from aiogram.filters import CommandStart
+from keyboards.main import inline_builder, reply_builder
+from db.model import User
 
+from aiogram import Router, F
+from aiogram.types import Message, CallbackQuery
+from aiogram.filters import CommandStart
+from aiogram.fsm.context import FSMContext
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
 user_router = Router()
 
 
 @user_router.message(CommandStart())
-async def default_handler(message: Message) -> NoReturn:
+async def default_handler(message: Message, session: AsyncSession, state: FSMContext) -> NoReturn:
     pattern = dict(
         text=f'Hello {message.from_user.full_name}',
         reply_markup=inline_builder(
